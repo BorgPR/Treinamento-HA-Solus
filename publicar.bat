739e53ae184993f305f1c36f343fb3eb2481824a
Paulo Ricardo Borges <paulo.borges@solus.inf.br>
2026-03-16 14:11:51 -0300

    fix: corrigida acentuação corrompida e protegido projetos.csv no deploy

--- publicar.bat
+++ publicar.bat
@@ -17,7 +17,7 @@ copy /Y "visaogeral_projetos.html" "\\172.16.80.15\WebTemp\infra\"
 copy /Y "dados-projetos.js" "\\172.16.80.15\WebTemp\infra\"
 copy /Y "gerenciamento.html" "\\172.16.80.15\WebTemp\infra\"
 copy /Y "dados-demandas.js" "\\172.16.80.15\WebTemp\infra\"
-copy /Y "projetos.csv" "\\172.16.80.15\WebTemp\infra\"
+rem copy /Y "projetos.csv" "\\172.16.80.15\WebTemp\infra\"
 
 echo.
 echo ==================================================
